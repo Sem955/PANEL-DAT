f3c3e661a6c55c710019fecce22bd5ebb33cd4ff
Game pengasil uang <140870382+Sem955@users.noreply.github.com>
2025-08-06 14:25:39 +0700

    Create supabase.js

---
+++ Supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = 'https://mppordupklxrmqhrtxrc.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcG9yZHVwa2x4cm1xaHJ0eHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NzQ3NjUsImV4cCI6MjA2ODQ1MDc2NX0.RtDugPVzWjIA6TjZmyXw3MI2oamApryHYmLArQq17Jw';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
